fcc6dcd918ba6dd0bbfb19399d219e1ba2d5f571
Félix Dorn <github@felixdorn.fr>
2021-05-31 17:56:17 +0200

    feat(swag): logo

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
 )
 
@@ -14,6 +15,19 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		fmt.Print(`
+$$\                           
+$$ |                          
+$$$$$$$\   $$$$$$\  $$$$$$$$\ 
+$$  __$$\ $$  __$$\ \____$$  |
+$$ |  $$ |$$$$$$$$ |  $$$$ _/ 
+$$ |  $$ |$$   ____| $$  _/   
+$$ |  $$ |\$$$$$$$\ $$$$$$$$\ 
+\__|  \__| \_______|\________|
+
+`)
+	},
 }
 
 func Execute() {
